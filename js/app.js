36e5a36e04bfa9ced295a6955ad5b75383de6ca9
Parmeshwar Gudadhe <param@mail.com>
2024-03-03 19:27:57 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -266,7 +266,7 @@ const progress = (() => {
         loaded += 1;
 
         bar.style.width = Math.min((loaded / total) * 100, 100).toString() + "%";
-        info.innerText = `Loading assets (${loaded}/${total}) [${parseInt(bar.style.width).toFixed(0)}%]`;
+        info.innerText = `Loading  Love ❤️(${loaded}/${total}) [${parseInt(bar.style.width).toFixed(0)}%]`;
 
         if (loaded == total) {
             util.show();
